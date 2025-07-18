a8cfec4a77302db75cf5640353efa50c82cf8be8
SajjadSj66 <sajjadjadidi666@gmail.com>
2025-07-19 01:10:42 +0330

    deploy

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
+#!/usr/bin/env bash
+
+# خروج اگر خطایی پیش اومد
+set -o errexit
+
+# نصب وابستگی‌ها
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# migrate
 python manage.py migrate
+
+# جمع‌آوری استاتیک‌ها
 python manage.py collectstatic --noinput
